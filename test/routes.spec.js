66eeb2a483cbb632383fc8a94ae9d2ae3da74996
amj133 <amj@vt.edu>
2018-05-04 18:23:49 -0600

    write post api/v1/foods test block

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,7 +3,7 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
 
-const environment = process.env.NODE_ENV || 'test';
+const environment = 'test';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
@@ -49,9 +49,7 @@ describe('API Routes', function() {
   })
 
   describe('GET /api/v1/foods/:id', function() {
-    this.timeout(0);
-
-    it('should return given food', function() {
+    xit('should return given food', function() {
       return chai.request(server)
       .get('/api/v1/foods/2')
       .then((response) => {
@@ -67,5 +65,29 @@ describe('API Routes', function() {
     })
   })
 
-  describe
+  describe('POST /api/v1/foods/', function () {
+    it('should create and return food', function () {
+      let payload = {
+        "food": {
+          "name": "Gogurt",
+          "calories": 220
+        }
+      }
+
+      return chai.request(server)
+      .post('/api/v1/foods')
+      .send(payload)
+      .then((response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('Object');
+
+        response.body[0].should.have.property('id');
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('Gogurt');
+        response.body[0].should.have.property('calories');
+        response.body[0].name.should.equal(220);
+      })
+    })
+  })
 })

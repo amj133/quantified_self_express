c988b9f7293f88d5f74301ea5f9d00e4ddc9d645
amj133 <amj@vt.edu>
2018-05-08 13:43:57 -0600

    add production directory under seeds directory

---
+++ db/seeds/production/foods.js
@@ -0,0 +1,19 @@
+exports.seed = function(knex, Promise) {
+  return knex.raw('TRUNCATE foods RESTART IDENTITY')
+  .then(async function() {
+    return Promise.all([
+      await knex.raw(
+        'INSERT INTO foods (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
+        ["Banana", 140, new Date, new Date]
+      ),
+      await knex.raw(
+        'INSERT INTO foods (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
+        ["Twizzler", 240, new Date, new Date]
+      ),
+      await knex.raw(
+        'INSERT INTO foods (name, calories, created_at, updated_at) VALUES (?, ?, ?, ?)',
+        ["Avocado", 340, new Date, new Date]
+      )
+    ])
+  })
+}
